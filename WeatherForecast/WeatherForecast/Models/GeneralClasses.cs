e063c96a4ec8c2414d51e65bcbad7fde555fdf22
Julia <shcherbak.yuliya@gmail.com>
2016-07-15 23:24:39 +0300

    Models and Views modified

---
+++ WeatherForecast/WeatherForecast/Models/GeneralClasses.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeatherForecast.Models
+{
+    public class City
+    {
+        public int CityID { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public Coord coord { get; set; }
+        public string country { get; set; }
+        public int population { get; set; }
+
+        public Sys sys { get; set; }
+    }
+    public class Coord
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+
+    public class Main
+    {
+        public double temp { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public double pressure { get; set; }
+        public double sea_level { get; set; }
+        public double grnd_level { get; set; }
+        public int humidity { get; set; }
+        public double temp_kf { get; set; }
+    }
+
+    public class Temp
+    {
+        public double day { get; set; }
+        public double min { get; set; }
+        public double max { get; set; }
+        public double night { get; set; }
+        public double eve { get; set; }
+        public double morn { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+    public class Clouds
+    {
+        public int all { get; set; }
+    }
+    public class Wind
+    {
+        public double speed { get; set; }
+        public double deg { get; set; }
+    }
+    public class Rain
+    {
+        public double rain { get; set; }
+    }
+    public class Snow
+    {
+        public double snow { get; set; }
+    }
+
+    public class Sys
+    {
+        public int population { get; set; }
+    }
+    public class Sys2
+    {
+        public string pod { get; set; }
+    }
+}
--- WeatherForecast/WeatherForecast/Models/WeekModel.cs
+++ WeatherForecast/WeatherForecast/Models/WeekModel.cs
@@ -7,7 +7,7 @@ namespace WeatherForecast.Models
 {
     public class WeekModel
     {
-        public WeekCity city { get; set; }
+        public City city { get; set; }
         public string cod { get; set; }
         public double message { get; set; }
         public int cnt { get; set; }
@@ -25,50 +25,19 @@ namespace WeatherForecast.Models
         }
     }
 
-    public class WeekCity
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public WeekCoord coord { get; set; }
-        public string country { get; set; }
-        public int population { get; set; }
-    }
-    public class WeekCoord
-    {
-        public double lon { get; set; }
-        public double lat { get; set; }
-    }
-
     public class WeekList
     {
+
         public int dt { get; set; }
         public DateTime date { get; set; }
-        public WeekTemp temp { get; set; }
+        public Temp temp { get; set; }
         public double pressure { get; set; }
         public int humidity { get; set; }
-        public List<WeekWeather> weather { get; set; }
+        public List<Weather> weather { get; set; }
         public double speed { get; set; }
         public int deg { get; set; }
         public int clouds { get; set; }
-        public double rain { get; set; }
-        public double snow { get; set; }
-    }
-
-    public class WeekTemp
-    {
-        public double day { get; set; }
-        public double min { get; set; }
-        public double max { get; set; }
-        public double night { get; set; }
-        public double eve { get; set; }
-        public double morn { get; set; }
-    }
-
-    public class WeekWeather
-    {
-        public int id { get; set; }
-        public string main { get; set; }
-        public string description { get; set; }
-        public string icon { get; set; }
+        public double? rain { get; set; }
+        public double? snow { get; set; }
     }
 }
